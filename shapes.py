b66e161bc47d17b4d989701314c9270fe0d47720
shazwendy <akothsharon19@gmail.com>
2019-04-30 19:15:55 +0300

    adding a file to this repository

---
+++ shapes.py
@@ -0,0 +1,68 @@
+class Circle:
+	def__init__(self,radius,area,circumference):
+		self.radius=radius
+		self.area=area
+		self.circumfrance=circumference
+		# self.pie=3.14
+
+
+	def area(self,radius,pie):
+		self.area=pie*(radius*radius)
+		print(area)
+
+	def circumference(radius,pie):
+		self.circumference=2(pie)*radius
+		print(circumference)
+
+
+
+class Square:
+	def__init__(self,side,area):
+		self.side=side
+		self.area=area
+		self.perimeter=perimeter
+
+	def area(self,side):
+		self.area=side*side
+		print(area)
+
+	def perimeter(self,side):
+		self.=4(side)
+		print(perimeter)
+
+
+class Rectangle
+	def__init__(self,w,l,A,P):
+		self.width=width
+		self.l=l
+		self.A=A
+		self.P=P
+
+	def area(self,w,l):
+		self.A=w*l
+		print(A)
+
+	def perimeter(self,w,l):
+		self.P=2(w+l)
+		print(P)
+
+
+
+class Sphere
+	def__init__(self,r,S_A,V)
+		self.r=r
+		self.S_A=S_A
+		self.V=V
+		self.pie=3.14
+
+	def SA(self,pie,r):
+		self.S_A=4(3.14*(r*r))
+		print(S_A)
+
+	def V(self,pie,r):
+		self.V=4/3*(3.14*(r*r*r))
+		
+
+
+
+
